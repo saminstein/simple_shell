c60cfe10e0fae296efee9fe6cf3817930b0ff75e
Sheyman0382 <adegbiteseyi22@gmail.com>
2023-09-18 08:50:59 +0000

    all prototypes for string operations updated

--- shell.h
+++ shell.h
@@ -19,10 +19,10 @@
 
 /**
  * struct builtin_c - builtin struct for command
- * @name: name of the command builtin (cd, exit, e
- * nv)
+ * @name: name of the command builtin (cd, exit, env)
  * @function: function that executes the command
  */
+
 typedef struct builtin_c
 {
 	char *name;
@@ -32,8 +32,10 @@ typedef struct builtin_c
 extern char **environ;
 char *_getenv(char *name);
 
+/** PRINTS **/
+void print(char *c);
 void prompt(void);
-
+void print_error(char **cmd, char *sms);
 
 
 /** GETLINE **/
@@ -45,8 +47,12 @@ char **tokenize(char *user_input);
 /** STRINGS **/
 int _strcmp(char *s1, char *s2);
 int _strlen(char *s);
-
-
+int _strncmp(const char *s1, const char *s2, size_t n);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+int _atoi(char *s);
+char *_strdup(char *str);
+void _memcpy(void *newptr, const void *ptr, size_t size);
 void *_realloc(void *ptr, size_t size);
 
 #endif
